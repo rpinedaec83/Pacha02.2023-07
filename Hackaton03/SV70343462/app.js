673580c0f83e211b4e51dd292d8d42074915d20c
jmendieta09 <jmendieta1309@gmail.com>
2023-08-04 18:25:42 -0500

    Problema_02

--- Hackaton03/SV70343462/app.js
+++ Hackaton03/SV70343462/app.js
@@ -1,11 +1,26 @@
 //1-Hacer un algoritmo en JavaScript que lea un número por el teclado y determinar si tiene tres dígitos.
 
+// const numero = prompt("Ingresar un número");
+
+// if (numero > 99 && numero < 1000) {
+//     document.querySelector('.resultado').innerHTML = `${numero} tiene 3 dígitos.`    
+// }
+// else {
+//     document.querySelector('.resultado').innerHTML = `${numero} no tiene 3 dígitos.`
+// }
+
+
+//Hacer un algoritmo en JavaScript que lea un número entero por el teclado y determinar si es negativo.
+
 const numero = prompt("Ingresar un número");
 
-if (numero > 99 && numero < 1000) {
-    document.querySelector('.resultado').innerHTML = `${numero} tiene 3 dígitos.`    
+if (numero < 0) {
+    document.querySelector('.resultado').innerHTML = `${numero} es negativo.`
+}
+else if (numero > 0) {
+    document.querySelector('.resultado').innerHTML = `${numero} es positivo.`
 }
 else {
-    document.querySelector('.resultado').innerHTML = `${numero} no tiene 3 dígitos.`
+    document.querySelector('.resultado').innerHTML = `${numero} es cero.`
 }
 
