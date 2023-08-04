a0d935d9d2d027a4335f1e92f2eccc2a22c2fc93
jmendieta09 <jmendieta1309@gmail.com>
2023-08-04 18:35:29 -0500

    Problema_03

--- Hackaton03/SV70343462/app.js
+++ Hackaton03/SV70343462/app.js
@@ -10,17 +10,30 @@
 // }
 
 
+
+
 //Hacer un algoritmo en JavaScript que lea un número entero por el teclado y determinar si es negativo.
 
-const numero = prompt("Ingresar un número");
+// const numero = prompt("Ingresar un número");
 
-if (numero < 0) {
-    document.querySelector('.resultado').innerHTML = `${numero} es negativo.`
-}
-else if (numero > 0) {
-    document.querySelector('.resultado').innerHTML = `${numero} es positivo.`
+// if (numero < 0) {
+//     document.querySelector('.resultado').innerHTML = `${numero} es negativo.`
+// }
+// else if (numero > 0) {
+//     document.querySelector('.resultado').innerHTML = `${numero} es positivo.`
+// }
+// else {
+//     document.querySelector('.resultado').innerHTML = `${numero} es cero.`
+// }
+
+
+//Hacer un algoritmo en JavaScript que lea un número y determinar si termina en 4.
+
+const numero = prompt("Ingresar un número")
+
+if (numero % 10 == 4) {// si el 'numero' dividido entre 10 tiene como residuo 4
+    document.querySelector('.resultado').innerHTML = `${numero} termina en 4.`
 }
 else {
-    document.querySelector('.resultado').innerHTML = `${numero} es cero.`
+    document.querySelector('.resultado').innerHTML = `${numero} no termina en 4.`
 }
-
