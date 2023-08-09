6dc9b9f65d978b81593c238b5dc6fc789d2e6a9b
Fabricio Oliva <foliva717@gmail.com>
2023-08-08 20:54:04 -0500

    Actualización de script

--- Semana04Sesion01/SV72808339/script.js
+++ Semana04Sesion01/SV72808339/script.js
@@ -29,3 +29,45 @@ let mi_helado2 = new Helados("Mega helado","Vainilla",12.49)
 
 mi_helado.vender()
 mi_helado2.vender()
+
+class Vehículos{
+    constructor(color, n_ruedas, motor, combustible){
+        this.color = color,
+        this.n_ruedas = n_ruedas,
+        this.motor = motor,
+        this.combustible = combustible
+    }
+
+    arrancar(){
+        console.log("El vehículo se ha prendido")
+    }    
+    acelerar(){
+        console.log(`Subiendo la velocidad`)
+    }
+    girar(direccion){
+        console.log(`Girando hacia la ${direccion}`)
+    }
+    frenar(){
+        console.log(`Bajando la velocidad`)
+    }
+    detener(){
+        console.log(`Deteniendo el vehículo por completo`)
+    }
+    describir(){
+        console.log(`Este es un auto de color ${this.color}, y tiene ${this.n_ruedas} ruedas`)
+    }
+    cargar_combustible(valor){
+        console.log(`Estoy cargando ${this.valor} soles de ${this.combustible} en mi auto de color ${this.color}`)
+    }
+}
+
+let susuki = new Vehículos("gris",4,"combustion interna","GLP");
+susuki.describir();
+susuki.arrancar();
+susuki.acelerar();
+susuki.girar("derecha");
+susuki.acelerar();
+susuki.girar("izquierda");
+susuki.frenar();
+susuki.detener()
+susuki.cargar_combustible(60)
