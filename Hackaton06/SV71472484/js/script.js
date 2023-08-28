63f59bbfd31a124fce481e575871cf48fe7ed601
Luis Bolivar <lbolivar20@gmail.com>
2023-08-27 23:29:49 -0500

    HAckaton 06

--- Hackaton06/SV71472484/js/script.js
+++ Hackaton06/SV71472484/js/script.js
@@ -6,33 +6,28 @@ const repararCelular = function () {
 
     function configurar(){
         $("#sistema").text(vSistema);
-        cargaSede();
         cargaUsuario();
         crear_telefonos();
         crear_tecnicos();
         crear_imei();
-        validarCelular();
-    }
-    function cargaSede(){
-        let strSede = localStorage.getItem("sede");
-        let objSede = JSON.parse(strSede);
-        if(strSede != null){
-            $("#modal-481741").text(`Sede: ${objSede.sede}`)
-        }
+        registarTelefonos();
+        cargarTelefonos();
+        //validarCelular();
     }
     function cargaUsuario(){
         let strLogin = sessionStorage.getItem("login");
         let objLogin = JSON.parse(strLogin);
         if(strLogin != null){
-            $("#modal-512127").text(`Hola ${objLogin.email}`)
+            $("#modal-512127").text(`Bienvenido: ${objLogin.email}`)
+            $("#sede").text(`Sede: ${objLogin.sede}`)
         }
     }
    function crear_telefonos(){
-    tel1 = new Telefono("MOTOROLA","MOTO G51","123456789","351202360424569")
+    tel1 = new Telefono(0,"MOTOROLA","MOTO G51","123456789","351202360424560","PANTALLA ROTA",500,"PANTALLA")
     telefonos.push(tel1);
-    tel2 = new Telefono("SAMSUNG","A52","012345678","331202461426569")
+    tel2 = new Telefono(1,"SAMSUNG","A52","012345678","331202461426569","PANTALLA ROTA",500,"PANTALLA")
     telefonos.push(tel2);
-    tel2 = new Telefono("XIAOMI","REDMI NOTE 12s","123406780","331602360454567")
+    tel2 = new Telefono(2,"XIAOMI","REDMI NOTE 12s","123406780","331204461426569","FALLA DE BATERIA",200,"BATERIA")
     telefonos.push(tel2);
    }
    function crear_tecnicos(){
@@ -44,7 +39,7 @@ const repararCelular = function () {
     tecnicos.push(teco3)
     }
     function crear_imei(){
-        repor1 = ("331202461426569")
+        repor1 = new Imeis ("331202461426569")
         imei.push(repor1);
        }
 
@@ -53,9 +48,11 @@ const repararCelular = function () {
             let formData = {
               email: $("#email").val(),
               pass: $("#pass").val(),
+              sede: $("#vSede").val(), 
              
             };          
             sessionStorage.setItem("login",JSON.stringify(formData));
+            sessionStorage.setItem("sede",JSON.stringify(formSede));
             event.preventDefault();
             cargaUsuario();
           });
@@ -70,57 +67,100 @@ const repararCelular = function () {
           });
     }
 
-    function validarCelular(){
-        nSerie = prompt("Escriba numero de serie")
-        nImei = prompt("Escriba numero de imei")
-        let verificarImei = new Telefono(nSerie,nImei)
-        let sSiNo = prompt("Validar con el portal de OSIPTEL y poner: \n 1 -> SI ESTA REPORTADO \n 2 -> NO ESTA REPORTADO")
-        if(verificarImei.verificaImei(sSiNo) == true){
-            alert("IMEI REPORTADO!!! NO SE PUEDE CONTINUAR")
-        }
-        else{
-            let diagnostico = prompt("Registar Primer Diagnostico")
-            verificarImei.rDiagnostico(diagnostico)
-            let total = prompt("Registar Total del Servicio")
-            let mitad = verificarImei.calcularMitad(total)
-            let autoriza = prompt(`Registrar Autorizacion y abono del 50%: ${mitad} \n 1 -> SI \n 2 -> NO`)
-            if(autoriza == '1'){
-                let marcaCel = prompt("Ingresar Marca \n 1 -> MOTOROLA \n 2-> SAMSUNG \n 3 -> XIAOMI")
-                verificarImei.asignarTecnico(tecnicos[marcaCel - 1])
-                let repuestos = prompt("Agregar Repuestos")
-                verificarImei.agregarRepuestos(repuestos)
-                mostrar_resultado(verificarImei)
-            } 
-            else{
-                alert("NO PROCEDE EL SERVICIO")
+    function registarTelefonos(){
+        telefonos.forEach(elemento => {
+            if(elemento.imei == imei[0].nImei){
+                telefonos[elemento.id].rReportado("SI");
+                telefonos[elemento.id].rDiagnostico("");
+                telefonos[elemento.id].calcularMitad(0);
+                telefonos[elemento.id].agregarRepuestos("");
+                telefonos[elemento.id].agregarEstado("REPORTADO")          
             }
-        }
+            else{
+                telefonos[elemento.id].rReportado("NO");
+                telefonos[elemento.id].calcularMitad(elemento.total);
+                telefonos[elemento.id].asignarTecnico(tecnicos[elemento.id])
+                telefonos[elemento.id].agregarEstado("REPARADO")
+            }
+            localStorage.setItem("telefonos",JSON.stringify(telefonos[elemento.id]));
+        });
     }
-
-    function mostrar_resultado(servicio){
-        console.log(servicio);
-
-        let marca = document.getElementById("marca")
-        marca.innerText =  servicio.tecnico.vMarcaTelefono;
-        let serie = document.getElementById("serie")
-        serie.innerText =  servicio.numeroSerie;
-        let imei = document.getElementById("imei")
-        imei.innerText =  servicio.imei;
-        let diagnostico = document.getElementById("diagnostico")
-        diagnostico.innerText =  servicio.diagnostico;
-        let costo = document.getElementById("costo")
-        costo.innerText =  servicio.total;
-        let abono = document.getElementById("abono")
-        abono.innerText =  servicio.mitad;
-        let tecnico = document.getElementById("tecnico")
-        tecnico.innerText =  servicio.tecnico.vNombres;
-        let repuestos = document.getElementById("repuestos");
-        repuestos.innerText =  servicio.repuestos;
-        let pago = document.getElementById("pago")
-        pago.innerText =  servicio.mitad;
-
-        let div_servicio =document.getElementById("div_servicio");
-        div_servicio.style.display  = "block";
+    function cargarTelefonos(){
+        $('#telefonos').empty();
+        let strHtml =`<table class="table table-sm">
+        <thead>
+            <tr>
+                <th>
+                    Marca
+                </th>
+                <th>
+                    Modelo
+                </th>
+                <th>
+                    Serie
+                </th>
+                <th>
+                    Imei
+                </th>
+                <th>
+                    Estado
+                </th>
+                <th>
+                    Reportado 
+                </th>
+                <th>
+                Tecnico
+            </th>
+            <th>
+            Total
+            </th>
+            <th>
+            Adelanto
+            </th>
+            <th>
+            Restante de Pago
+            </th>
+            </tr>
+        </thead>
+        <tbody>`
+        telefonos.forEach(element => {
+          strHtml +=  `<tr>
+          <td>
+              ${element.marca}
+          </td>
+          <td>
+              ${element.modelo}
+          </td>
+          <td>
+              ${element.numeroSerie}
+          </td>
+          <td>
+              ${element.imei}
+          </td>
+          <td>
+            ${element.estado}
+         </td>
+          <td>
+           ${element.reportado}
+          </td>
+          <td>
+          ${element.tecnico}
+         </td>
+         <td>
+         ${element.total}
+        </td>
+        <td>
+        ${element.mitad}
+       </td>
+       <td>
+       ${element.mitad}
+      </td>
+      </tr>
+      <tr><td id ="detalle_${element.id}" ></td></tr>`
+        });
+        strHtml += `</tbody>
+        </table>`;
+        $('#telefonos').html(strHtml);
     }
 
     return {
@@ -133,23 +173,22 @@ const repararCelular = function () {
 }();
 
 class Telefono{
-    constructor(marca, modelo,numeroSerie, imei){
+    constructor(id,marca,modelo,numeroSerie, imei,diagnostico,total,repuestos){
+        this.id = id;
         this.marca = marca;
         this.modelo = modelo;
         this.numeroSerie = numeroSerie;
         this.imei = imei;
-        this.diagnostico ='';
-        this.total = 0;
+        this.diagnostico = diagnostico;
+        this.total = total;
+        this.repuestos = repuestos;
+        this.estado = 'Pendiente';
+        this.reportado ='';       
         this.mitad = 0;
-        this.tecnico = null;
-        this.repuestos ='';
+        this.tecnico = null;  
         }
-        verificaImei(sino){
-           if(sino == '1'){ 
-            return true;
-           }else{
-            return false;
-           }
+        rReportado(reportado){
+            this.reportado = reportado;
         }
         rDiagnostico(diagnostico){
             this.diagnostico = diagnostico
@@ -157,7 +196,6 @@ class Telefono{
         calcularMitad(total) {
             this.total = total
             this.mitad = total/2;
-            return this.mitad
         }
         asignarTecnico(tecnico){
             this.tecnico = tecnico
@@ -165,7 +203,9 @@ class Telefono{
         agregarRepuestos(repuestos){
             this.repuestos = repuestos
         }
-
+        agregarEstado(estado){
+            this.estado = estado
+        }
 }
 class Tecnicos{
     constructor(tCodigo,vNombres,iCodTel,vMarcaTelefono){
@@ -173,5 +213,10 @@ class Tecnicos{
         this.vNombres = vNombres;
         this.iCodTel = iCodTel;
         this.vMarcaTelefono = vMarcaTelefono;
+    }
+}
+class Imeis{
+    constructor(nImei){
+        this.nImei= nImei;
     }
 }
