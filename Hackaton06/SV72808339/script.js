78c48c76beac9e64bd02b01eff46487d7bfc29c9
Fabricio Oliva <foliva717@gmail.com>
2023-08-27 21:13:00 -0500

    Hackathon 06

--- Hackaton06/SV72808339/script.js
+++ Hackaton06/SV72808339/script.js
@@ -1,13 +1,25 @@
 class Sucursal {
     constructor(nombre){
         this.nombre = nombre;
-        this.telefonos = []
+        this.celulares = []
         this.tecnicos = []
+        this.sucursales = []
     }
 
-    agregar_telefono(telefono){
-        this.telefonos.push(telefono)
-        console.log(`El teléfono ${telefono.marca} ${telefono.modelo} ha sido añadido a la sucursal ${this.nombre}`)
+    static crear_sucursal(nombre) {
+        if (Sucursal.sucursales.includes(nombre)) {
+            console.log("Esta sucursal ya ha sido registrada con anterioridad.");
+            return null;
+        } else {
+            const suc = new Sucursal(nombre);
+            Sucursal.sucursales.push(suc);
+            return suc;
+        }
+    }
+
+    agregar_celular(celular){
+        this.celulares.push(celular)
+        console.log(`El teléfono ${celular.marca} ${celular.modelo} ha sido añadido a la sucursal ${this.nombre}`)
     }
 
     agregar_tecnico(tecnico){
@@ -16,12 +28,13 @@ class Sucursal {
     }
 }
 
-class Telefono {
-    constructor(serie, imei, marca, modelo) {
+class Celular {
+    constructor(serie, imei, marca, modelo, sucursal) {
         this.serie = serie;
         this.imei = imei;
         this.marca = marca;
         this.modelo = modelo;
+        this.sucursal = sucursal;
         this.autorizacion = false;
         this.pago_parcial = false;
 
@@ -34,14 +47,14 @@ class Telefono {
     static registro_series = [];
     static registro_imei = [];
 
-    static crear_telefono(serie, imei, marca, modelo) {
-        if (Telefono.reg_series.includes(serie) || Telefono.reg_imei.includes(imei)) {
+    static crear_celular(serie, imei, marca, modelo) {
+        if (Celular.reg_series.includes(serie) || Celular.reg_imei.includes(imei)) {
             console.log("Este teléfono ya ha sido registrado.");
             return null;
         } else {
-            const tel = new Telefono(serie, imei, marca, modelo);
-            Telefono.reg_series.push(serie);
-            Telefono.reg_imei.push(imei);
+            const tel = new Celular(serie, imei, marca, modelo);
+            Celular.reg_series.push(serie);
+            Celular.reg_imei.push(imei);
             return tel;
         }
     }
@@ -116,9 +129,9 @@ class Telefono {
         if (this.pago_parcial && this.autorizacion){
             if(this.diagnosticos.length != 0){
                 this.estado = 3; //1 = en revisión, 2 = diagnosticado, 3 = en reparación, 4 = reparado
-                console.log(`El telefono está siendo reparado y ${this.tecnico_encargado.nombre} es el técnico encargado de repararlo.`)
+                console.log(`El celular está siendo reparado y ${this.tecnico_encargado.nombre} es el técnico encargado de repararlo.`)
             } else {
-                console.log("El telefono aún no ha sido diagnosticado.")
+                console.log("El celular aún no ha sido diagnosticado.")
             } 
         } else {console.log("No se ha dado la autorización del usuario o el pago del 50%")}
     }
@@ -126,17 +139,18 @@ class Telefono {
     registrar_reparo(){
         if(this.diagnosticos.length != 0){
             this.estado = 4; //1 = en revisión, 2 = diagnosticado, 3 = en reparación, 4 = reparado
-            console.log(`El telefono ha sido reparado con éxito y ${this.tecnico_encargado.nombre} fue el técnico encargado.`)
+            console.log(`El celular ha sido reparado con éxito y ${this.tecnico_encargado.nombre} fue el técnico encargado.`)
         } else {
-            console.log("El telefono aún no ha sido diagnosticado.")
+            console.log("El celular aún no ha sido diagnosticado.")
         }
     }
 }
 
 class Tecnicos {
-    constructor(nombre,experiencia,...especializaciones){
+    constructor(nombre,experiencia,sucursal,...especializaciones){
         this.nombre = nombre;
         this.experiencia = experiencia;
+        this.sucursal = sucursal;
         this.especializaciones = especializaciones;
     }
 
