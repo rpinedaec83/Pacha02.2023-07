61a681b20aac9ceb9f8898af72cc3476198a8d94
IROJASC <irojasc3@gmail.com>
2023-09-05 16:50:57 -0500

    Actualiza valores del gitignore

--- Hackaton07/SV72732900/apiModule.js
+++ Hackaton07/SV72732900/apiModule.js
@@ -1,6 +1,8 @@
 // "No se encontro el tipo de cambio de la moneda peruana dentro de la API"
 const urlExchange = "https://api.frankfurter.app/latest?from?amount=1&from=USD&to=EUR"
 const urlPokemons = "https://pokeapi.co/api/v2/pokemon"
+const urlRickAndMorty = "https://rickandmortyapi.com/api/character"
+client_id = "u-e65BiFrOncTgkSm7_iFfjojSkQOiv8qxxr_V45EO4"
 
 exports.getExchange = async () => {
     const res = await fetch(urlExchange);
@@ -30,8 +32,59 @@ exports.getPokemons = async (name = "") => {
             return false
         }
         else{
-            pokemonsNames = data.results.map((e, index)=>({"id": index.toString() ,"name": e.name}))
+            pokemonsNames = data.results.map((e, index)=>({"id": index.toString(),"name": e.name}))
             return JSON.stringify(pokemonsNames)
         }
     }
 }
+
+exports.getRickAnMorty = async (id = null) => {
+    if(!!id){
+        const res = await fetch(urlRickAndMorty+`/${id}`);
+        if (res.status !== 200){
+            return false
+        }
+        else{
+            const data = await res.json();
+            return JSON.stringify(data)
+        }
+    }else{
+        const res = await fetch(urlRickAndMorty, {method: "GET"});
+        const data = await res.json();
+        if (res.status !== 200){
+            return false
+        }
+        else{
+            let RickAndMorty = data.results.map((e, index)=>({"id": e.id, "name": e.name}))
+            return JSON.stringify(RickAndMorty)
+        }
+    }
+}
+
+exports.getImage = async (pattern = null, w = null, h = null) => {
+    
+    if(!!pattern){
+        if(!!w && !!h){
+            const res = await fetch(`https://api.unsplash.com/search/photos?query=${pattern}&per_page=1&client_id=${client_id}`);
+            const data = await res.json();
+            if (res.status !== 200){
+                console.log(data.message)
+                return false
+            }else{
+                return JSON.stringify({"custom_link": `https://source.unsplash.com/${data.results[0].id}/${w}x${h}`})
+            }
+        }else{
+            const res = await fetch(`https://api.unsplash.com/search/photos?query=${pattern}&per_page=1&client_id=${client_id}`);
+            const data = await res.json();
+            if (res.status !== 200){
+                console.log(data.message)
+                return false
+            }else{
+                return JSON.stringify({"custom_link": `https://source.unsplash.com/${data.results[0].id}`})
+            }
+        }
+    }
+    else{
+        return false;
+        }
+    }
--- Hackaton07/SV72732900/index.js
+++ Hackaton07/SV72732900/index.js
@@ -1,7 +1,10 @@
 
 let http = require('http');
 let url = require('url')
-let api  = require("./apiModule");  
+let api  = require("./apiModule");
+let fs = require('fs');  
+let cheerio = require('cheerio')
+// $('img').replaceWith(`<span>ivan</span>`)
 
 http.createServer(async function (req, res){
   let objURL = url.parse(req.url, true)
@@ -26,6 +29,51 @@ http.createServer(async function (req, res){
         res.end();
         break;
       }else{
+        res.writeHead(404, {'Content-Type': 'application/json'});
+        res.end("Not Found");
+        break;
+      }
+    case "/rickandmorty":
+      if (!objURL.query.id){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        rickAndmorty = await api.getRickAnMorty()
+        !!rickAndmorty && res.write(rickAndmorty)
+        res.end();
+        break;
+      }
+      else if(!!objURL.query.id){
+        rickAndmorty = await api.getRickAnMorty(objURL.query.id);
+        !!rickAndmorty ? res.write(rickAndmorty) : res.write("Not Found")
+        res.end();
+        break;
+      }else{
+        res.writeHead(404, {'Content-Type': 'application/json'});
+        res.end("Not Found");
+        break;
+      }
+    case "/imagenes":
+      if (!objURL.query.tema){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.write("Not Found")
+        res.end();
+        break;
+      }else if(!!objURL.query.tema){
+        if(!!objURL.query.ancho && !!objURL.query.alto){
+          res.writeHead(200, {'Content-Type': 'application/json'});
+          imgLink = await api.getImage(objURL.query.tema,objURL.query.ancho, objURL.query.alto);
+          !!imgLink ? res.write(imgLink) : res.write("Not Found");
+          res.end();
+          break;
+        }else{
+          res.writeHead(200, {'Content-Type': 'application/json'});
+          imgLink = await api.getImage(objURL.query.tema);
+          !!imgLink ? res.write(imgLink) : res.write("Not Found")
+          res.end();
+          break;
+        }
+      }
+      else{
         res.writeHead(404, {'Content-Type': 'application/json'});
         res.end("Not Found");
         break;
