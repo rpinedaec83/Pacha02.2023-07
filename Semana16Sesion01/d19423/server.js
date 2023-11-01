44864b7e4d819a7b71b5da41517d4f101d0323bb
Roberto Pineda <rpineda@x-codec.net>
2023-10-31 21:29:34 -0500

    Clase 31102023 jwt

---
+++ Semana16Sesion01/d19423/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+require('dotenv').config()
+
+
+const app = express();
+
+// cors
+const cors = require('cors');
+var corsOptions = {
+    origin: '*', // Reemplazar con dominio
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
+
+// capturar body
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+// Conexión a Base de datos
+let uri = process.env.URI;
+mongoose.connect(uri,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+)
+.then(() => console.log('Base de datos conectada'))
+.catch(e => console.log('error db:', e))
+
+
+const authRoutes = require('./routes/auth');
+
+// route middlewares
+app.use('/api/user', authRoutes);
+
+// import routes
+const dashboadRoutes = require('./routes/dashboard');
+const verifyToken = require('./routes/validate-token');
+
+// route middlewares
+app.use('/api/dashboard', verifyToken, dashboadRoutes);
+// route middlewares
+app.get('/', (req, res) => {
+    res.json({
+        estado: true,
+        mensaje: 'funciona!'
+    })
+});
+
+// iniciar server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`servidor andando en: ${PORT}`)
+})
